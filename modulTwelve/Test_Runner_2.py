ded0752fa40f416aa0de2776cb6ca98b306e6b69
lonelorth <109285932+lonelorth@users.noreply.github.com>
2024-09-29 20:38:21 +0300

    Add files via upload

--- modulTwelve/Test_Runner_2.py
+++ modulTwelve/Test_Runner_2.py
@@ -1,35 +1,43 @@
-import Runner_2 as runner
+from tests_12_3 import Runner, Tournament
 import unittest
 
 class TournamentTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.all_results = {}
+        cls.all_results = []
 
     def setUp(self):
-        vs = {'Усэйн': 10, 'Андрей': 9, 'Ник': 3}
-        self.runners = {n: runner.Runner(name=n, speed=v) for n, v in vs.items()}
+        self.usain = Runner("Усэйн", 10)
+        self.andrey = Runner("Андрей", 9)
+        self.nick = Runner("Ник", 3)
 
     @classmethod
     def tearDownClass(cls):
-        for k, v in cls.all_results.items():
-            print(f'{k}: {v}')
+        for result in cls.all_results:
+            formatted_result = '{' + ', '.join(f'{place}: {runner}' for place, runner in result.items()) + '}'
+            print(formatted_result)
 
-    def test_tournament_1(self):
-        tour = runner.Tournament(90, self.runners['Усэйн'], self.runners['Ник'])
-        all_results = tour.start()
-        self.assertTrue(all_results[2], self.runners['Ник'])
+    def test_usain_nick(self):
+        tournament = Tournament(90, self.usain, self.nick)
+        results = tournament.start()
+        self.all_results.append(results)
+        last_runner = results[max(results.keys())]
+        self.assertTrue(last_runner == "Ник")
 
-    def test_tournament_2(self):
-        tour = runner.Tournament(90, self.runners['Андрей'], self.runners['Ник'])
-        all_results = tour.start()
-        self.assertTrue(all_results[2], self.runners['Ник'])
+    def test_andrey_nick(self):
+        tournament = Tournament(90, self.andrey, self.nick)
+        results = tournament.start()
+        self.all_results.append(results)
+        last_runner = results[max(results.keys())]
+        self.assertTrue(last_runner == "Ник")
 
-    def test_tournament_3(self):
-        tour = runner.Tournament(90, self.runners['Усэйн'], self.runners['Андрей'], self.runners['Ник'])
-        all_results = tour.start()
-        self.assertTrue(all_results[3], self.runners['Ник'])
+    def test_usain_andrey_nick(self):
+        tournament = Tournament(90, self.andrey, self.usain, self.nick)
+        results = tournament.start()
+        self.all_results.append(results)
+        last_runner = results[max(results.keys())]
+        self.assertTrue(last_runner == "Ник")
 
 if __name__ == '__main__':
     unittest.main()
